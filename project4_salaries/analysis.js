1ca807a941a0edcf3092ad3689dfbe031f57b010
Javier Gamez <javier.rgb@hotmail.com>
2021-10-31 13:58:09 -0700

    salaries analysis JS, completed

--- project4_salaries/analysis.js
+++ project4_salaries/analysis.js
@@ -1,3 +1,10 @@
+// Helpers
+function is_even(num){
+    return (num % 2 == 0);
+}
+
+
+// Creating & sorting an Array with the salaries
 const salaries_all = colombia.map(
     function(person) {
         return person.salary;
@@ -10,10 +17,12 @@ const salaries_sorted = salaries_all.sort(
     }
 );
 
-function is_even(num){
-    return (num % 2 == 0);
-}
+splice_start = (salaries_sorted.length * 90) / 100;
+splice_qty = salaries_sorted.length - splice_start;
+salaries_top10 = salaries_sorted.splice(splice_start, splice_qty);
 
+
+// MAIN
 function get_median(lista){
     const middle = parseInt(lista.length / 2);
 
@@ -25,4 +34,8 @@ function get_median(lista){
     }
 }
 
-console.log(get_median(salaries_all));
+median = get_median(salaries_all);
+
+median_top10 = get_median(salaries_top10);
+
+console.log(median, median_top10);
